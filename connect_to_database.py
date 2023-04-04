779094d10ea78499270faac632f05425eae5c7d0
KO14ST57YA <120173988+KO14ST57YA@users.noreply.github.com>
2023-04-04 12:54:10 +0300

    Add files via upload

--- connect_to_database.py
+++ connect_to_database.py
@@ -2,14 +2,26 @@
 режимы открытия файла:
 
 """
-
-def write_to_db(grade, name, photo):
-    filename = 'text.txt'
-
+filename = 'text.txt'
+def write_to_db(user_id, grade, name, photo):
     with open(filename, 'a') as file:
+        user_id = str(user_id)
+        file.write(user_id)
+        file.write('\t')
         file.write(grade)
         file.write('\t')
         file.write(name)
         file.write('\t')
         file.write(photo)
-        file.write('\n')
+        file.write("\n")
+
+def get_user_from_db(my_user_id):
+    with open(filename, 'r') as file:
+        for line in file:
+            user_id, grade, name, photo = line.strip().split('\t')
+            if str(my_user_id) == user_id:
+                return {'user_id': user_id, 'class': grade, 'name': name, 'photo': photo}
+
+if __name__ == '__main__':
+    print(get_user_from_db(856340203))
+
